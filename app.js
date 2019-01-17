3541912a280aec2e4da32cc28ac0adca7da7b601
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-17 11:58:35 -0700

    remove semis, change var to const

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
 
 // might use, see: https://www.npmjs.com/package/http-errors
-// const createError = require('http-errors');
+// const createError = require('http-errors')
 
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
 // pull in routers
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users/router');
-
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users/router')
+const narrativesRouter = require('../routes/narratives/router')
 // boilerplate
-var app = express();
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const app = express()
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 // setup routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/', narrativesRouter)
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // ===========================================================
 // Error handler for next(object) / 500
 // ===========================================================
 app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  console.log("======================= APP ERROR IN CONTROLLER =======================");
-  console.log('status: ',status);
-  console.log('-------');
-  console.log('raw error: ',err);
-  console.log("^^^^^^^^^^^^^^^^^^^^^^^ APP ERROR IN CONTROLLER ^^^^^^^^^^^^^^^^^^^^^^");
-  res.status(status).json('' + err);
-  // next();
-});
+  const status = err.status || 500
+  console.log("======================= APP ERROR IN CONTROLLER =======================")
+  console.log('status: ',status)
+  console.log('-------')
+  console.log('raw error: ',err)
+  console.log("^^^^^^^^^^^^^^^^^^^^^^^ APP ERROR IN CONTROLLER ^^^^^^^^^^^^^^^^^^^^^^")
+  res.status(status).json('' + err)
+  // next()
+})
 
-module.exports = app;
+module.exports = app
