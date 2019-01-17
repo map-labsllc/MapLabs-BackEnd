cbe16bf0fb4858ac3a41917ff9b7eb95162c1a35
Ed Phelps <edphelps1@gmail.com>
2019-01-17 09:38:37 -0700

    added constraint and fixed seeds

--- migrations/20190117061702_narratives.js
+++ migrations/20190117061702_narratives.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
     table.increments("narrative_id").primary()
     table.integer('narrative_code').notNullable()
     table.integer('user_id').notNullable()
+    table.unique(['narrative_code', 'user_id'])
     table.foreign('user_id').references('users.user_id').onDelete('cascade')
     table.text('narrative').notNullable()
   })
