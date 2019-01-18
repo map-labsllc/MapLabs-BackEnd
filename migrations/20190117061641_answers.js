48aaac5fd9130f83d9f8ed6727029b8c50a125fc
Ed Phelps <edphelps1@gmail.com>
2019-01-18 06:52:03 -0700

    changed field order

--- migrations/20190117061641_answers.js
+++ migrations/20190117061641_answers.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('answers', function(table) {
     table.increments("answer_id").primary()
-    table.integer('question_code').notNullable()
     table.integer('user_id').notNullable()
+    table.integer('question_code').notNullable()
     table.text('answer').notNullable()
 
     table.foreign('user_id').references('users.user_id').onDelete('cascade')
