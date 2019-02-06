349db3deb7872ce8513ba438e7067db3bbac9665
Nick Griffen <nicholas.s.griffen@gmail.com>
2019-02-05 18:34:44 -0700

    Update app.js
    heroku can't handle RSA key the same way as local machine

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY
+    privateKey: JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
   }),
   databaseURL: process.env.FIREBASE_DB_URL
 })
