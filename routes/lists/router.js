0c341afb43b2a37d986d65e151f3e48f8a2678e7
Craig Quincy <craig.quincy@techstars.com>
2020-09-09 09:09:26 -0600

    no need for individual lists

--- routes/lists/router.js
+++ routes/lists/router.js
@@ -33,32 +33,4 @@ router.get('/', (req, res, next) => {
 
 });
 
-router.get('/:list_name', (req, res, next) => {
-  let { list_name } = req.params
-  console.log(`GET lists/${list_name}`);
-
-  // validate params
-  if (!list_name) {
-    const errMsg = `Bad list_name param to GET /lists: ${list_name}`
-    console.log("ERROR", errMsg)
-    throw new Error(errMsg)
-  }
-
-  knex('options')
-    .select('options.id', 'options.value')
-    .join('lists', {'lists.id': 'options.list_id'})
-    .where('lists.name', list_name)
-    .orderBy(['options.sort, options.value'])
-    .returning('*')
-    .then((data) => {
-      console.log(data)
-      return res.status(200).json(data)
-    })
-    .catch((error) => {
-      console.log('caught error ', error);
-      next(error);
-    });
-
-});
-
 module.exports = router;
