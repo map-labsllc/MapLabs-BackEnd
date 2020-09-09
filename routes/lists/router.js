2230978ea5c6d6082d5d31896c2d4df6b7809577
Craig Quincy <craig.quincy@techstars.com>
2020-09-09 08:52:01 -0600

    return all lists from index

--- routes/lists/router.js
+++ routes/lists/router.js
@@ -4,6 +4,35 @@ const router = express.Router();
 const knex = require('../../knex');
 const { checkUserPermissions } = require('../authMiddleware');
 
+// return all lists
+router.get('/', (req, res, next) => {
+  console.log(`GET lists`);
+
+  knex('options')
+    .select('lists.name', 'options.id', 'options.value')
+    .join('lists', {'lists.id': 'options.list_id'})
+    .orderBy(['lists.name', 'options.sort', 'options.value'])
+    .returning('*')
+    .then((data) => {
+      console.log(data)
+
+      // group by list
+      let lists = data.reduce((acc, row) => {
+        let {name, id, value} = row
+        if (!acc[name]) acc[name] = []
+        acc[name].push({id, value})
+        return acc
+      }, {})
+
+      return res.status(200).json(lists)
+    })
+    .catch((error) => {
+      console.log('caught error ', error);
+      next(error);
+    });
+
+});
+
 router.get('/:list_name', (req, res, next) => {
   let { list_name } = req.params
   console.log(`GET lists/${list_name}`);
