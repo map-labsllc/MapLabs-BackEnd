d31362e253e339b99ae1613990d3fa5ad13ca839
Craig Quincy <craig.quincy@techstars.com>
2020-09-27 16:00:27 -0600

    Revert "check user auth permissions on GET user"
    This reverts commit 883b0814b0c548abd669ca276bff308483038c8a.

--- routes/users/router.js
+++ routes/users/router.js
@@ -34,11 +34,18 @@ login_service_id is not pulled out of the URL before being used
 
 
 ***************************************************** */
-router.get('/', checkUserPermissions, (req, res, next) => {
+router.get('/', (req, res, next) => {
   console.log('GET users/:login_service_id/:login_token');
 
   // get jwt from header, if no jwt then not authorized
   const jwt = req.get('Authorization').replace('Token:', '').trim();
+  console.log('headers', req.headers);
+  if (!jwt) {
+    const error = new Error('Unauthorized');
+    error.status = 422;
+    return next(error);
+  }
+
 
   // normal auth process
   return admin.auth().verifyIdToken(jwt).then((decodedJwt) => {
